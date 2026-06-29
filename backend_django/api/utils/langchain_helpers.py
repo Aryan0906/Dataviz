1c3d90fbd68ab1b3aa49c1a14279fdaac2aad04c
Modh Vyom Dineshkumar <160481675+DarkHeaVen1711@users.noreply.github.com>
2026-06-30 00:44:18 +0530

    fix(A1): remove unused langchain.chains import

--- backend_django/api/utils/langchain_helpers.py
+++ backend_django/api/utils/langchain_helpers.py
@@ -3,7 +3,6 @@ LangChain Integration - Structured LLM interactions for data queries
 """
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import ChatPromptTemplate
-from langchain.chains import LLMChain
 import os
 import pandas as pd
 from typing import Dict, Any
